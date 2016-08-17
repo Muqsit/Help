d57aaa2142883a7a060c7eaaa0be64dc22b0cb7d
Muqsit Rayyan <hackhack05@gmail.com>
2016-08-17 18:11:43 +0300

    Fix undefined offsets. The console is clean now!

--- src/Muqsit/Main.php
+++ src/Muqsit/Main.php
@@ -25,13 +25,13 @@ class Main extends PluginBase implements Listener{
       $protect = $this->helpcfg->get("pages");
 
       if($command[0] === "/help" or $command[0] === "/?"){
-      if($command[1] > 0 && $command[1] < $protect + 1){
+      if(isset($command[1]) && $command[1] > 0 && $command[1] < $protect + 1){
         $messages = $this->helpcfg->get("page." . $command[1]);
          foreach($messages as $msges){
             $player->sendMessage($msges);
             $event->setCancelled();
             }
-         }elseif($command[1] < 1 or $command[1] > $protect){
+         }elseif(!isset($command[1]) or $command[1] < 1 or $command[1] > $protect){
           $helpInitial = $this->helpcfg->get("page.1");
                foreach($helpInitial as $helpInit){
                $player->sendMessage($helpInit);
