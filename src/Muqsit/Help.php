06903edae60e6cc29684575aa20db1623f7004c2
Muqsit Rayyan <hackhack05@gmail.com>
2016-10-21 21:27:17 +0300

    Performance Update!
    * Removed PlayerCommandPreprocessEvent
    * Clean of hacks
    * Multiple plugin support

---
+++ src/Muqsit/Help.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Muqsit;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\Server;
+
+class Help extends PluginBase {
+
+  public function onEnable(){
+    if(!file_exists($this->getDataFolder() . "help.yml")) {
+      @mkdir($this->getDataFolder());
+      file_put_contents($this->getDataFolder() . "help.yml",$this->getResource("help.yml"));
+    }
+    $this->saveDefaultConfig();
+    $this->getServer()->getCommandMap()->getCommand("help")->setLabel("help_not_anymore");
+    $this->getServer()->getCommandMap()->getCommand("help")->unregister($this->getServer()->getCommandMap());
+    $this->help = new Config($this->getDataFolder() . "help.yml", Config::YAML);
+  }
+}
